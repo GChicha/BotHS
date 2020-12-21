33658ab1995e6a01e327ff16a485f8efe5adce51
Luishfs <luish1980@hotmail.com>
2020-12-21 13:13:25 -0300

    added basic filters

--- Bot/bot.py
+++ Bot/bot.py
@@ -17,7 +17,20 @@ def regras(update: Update, context: CallbackContext):
 def help(update: Update, context: CallbackContext):
     resposta = "Colocar texto aqui"
     update.message.reply_text(resposta)
+    
+# POSSIVEIS ERROS:
+#Secção criada para que, caso o usuário escreva um comando comando errado
+#Ou tente conversar com o Bot
 
+def comando_Errado(update: Update, context: CallbackContext):
+    resposta = "Esse comando não existe. Favor checar a digitação!"
+    update.message.reply_text(resposta)
+
+def conversa_paralela(update: Update, context: CallbackContext):
+    resposta = "Sou um Robô!!!, não tenho assunto. Favor utilizar os comandos!"
+    update.message.reply_text(resposta)
+
+#Código Principal    
 def main():
     #Esse updater puxa nosso bot da API do telegram, esse token
     #é entregue quando criamos o bot.
@@ -36,7 +49,15 @@ def main():
     dispatcher.add_handler(
         CommandHandler('help', help)
     )
-
+    #Faz o bot receber a mensagem e processar uma resposta
+    dispatcher.add_handler(
+        #filters filtrará o que foi especificado
+        MessageHandler(Filters.command, comando_Errado)
+    )
+    dispatcher.add_handler(
+        #filters filtrará o que foi especificado
+        MessageHandler(Filters.all, conversa_paralela)
+    )
     updater.start_polling()
 
     updater.idle()
